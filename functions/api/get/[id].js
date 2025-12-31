fe727d89b9f68203a571a55504ab564cb33a6a52
yogee20001 <ykyadav20001@gmail.com>
2025-12-31 16:04:28 +0530

    Update [id].js

--- functions/api/get/[id].js
+++ functions/api/get/[id].js
@@ -1,25 +1,5 @@
-export async function onRequest(context) {
-  try {
-    const { params, env } = context;
-
-    if (!env.GREETINGS_KV) {
-      throw new Error("KV binding GREETINGS_KV not found");
-    }
-
-    const data = await env.GREETINGS_KV.get(params.id);
-
-    if (!data) {
-      return new Response("Not found", { status: 404 });
-    }
-
-    return new Response(data, {
-      headers: { "Content-Type": "application/json" }
-    });
-  } catch (err) {
-    return new Response(
-      JSON.stringify({ error: err.message }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
-  }
+export async function onRequest() {
+  return new Response("FUNCTION OK");
 }
+
 
