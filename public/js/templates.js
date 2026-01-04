3be28713e96d50b7e0c5dc05bb1cfda0e1da1e17
yogee20001 <ykyadav20001@gmail.com>
2026-01-04 15:20:53 +0530

    Add files via upload

---
+++ public/js/templates.js
@@ -0,0 +1,30 @@
+const templates = {
+  birthday: [
+    {
+      id: "bday_01",
+      title: "Birthday Sparkle",
+      cover: "/assets/templates/bday1.webp"
+    },
+    {
+      id: "bday_02",
+      title: "Golden Birthday",
+      cover: "/assets/templates/bday2.webp"
+    }
+  ],
+
+  "new-year": [
+    {
+      id: "ny_01",
+      title: "New Year Blast",
+      cover: "/assets/templates/ny1.webp"
+    }
+  ],
+
+  anniversary: [
+    {
+      id: "ann_01",
+      title: "Forever Love",
+      cover: "/assets/templates/ann1.webp"
+    }
+  ]
+};
